1e2c3ea6984ac9d8e24c85c229dc347176a7910d
AFulle <afuller@csumb.edu>
2016-12-09 11:09:32 -0800

    Create lab16_LastName.py

---
+++ lab16_LastName.py
@@ -0,0 +1,27 @@
+# CST 205 Lab 16: URLS and HTMLS
+# Yulian
+# Lyndsay
+# Ahdia
+
+# warm up
+def makePage():
+  #replace the directory in the line below with the path to your file
+  file = open("C:\\Users\\sag12\\Desktop\\Python\\webFile.html", "wt")
+  file.write("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 
+  Transition//EN" "http://www.w3.org/TR/html4/loose.dtd">
+  
+  <html>
+  <head><title>I made this page with Python!</title>
+  </head>
+  <body>
+  <h1>MY PYTHON PAGE!!!</h1>
+  </body>
+  </html>""")
+  
+  file.close()
+  
+  # code here for scraping text of frequently used webpage
+  
+  # code here for collecting 10 pieces of information from previous webpage
+  
+  #code here for creating a new web page that only displays information collected
