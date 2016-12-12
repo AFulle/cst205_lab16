700664fdc2258bdacce9338130803fcf6fe0ceaa
Yulian Kuncheff <yulian@kuncheff.com>
2016-12-12 00:22:47 -0800

    Create ars technica parser

--- lab16_LastName.py
+++ lab16_LastName.py
@@ -3,6 +3,10 @@
 # Lyndsay
 # Ahdia
 
+import urllib
+import re
+import os
+
 # warm up
 def makePage():
   #replace the directory in the line below with the path to your file
@@ -21,7 +25,34 @@ def makePage():
   file.close()
   
   # code here for scraping text of frequently used webpage
-  
-  # code here for collecting 10 pieces of information from previous webpage
+def parse_arstechnica_homepage():
+  verge_homepage = urllib.urlopen('https://arstechnica.com').read()
+  matches = re.findall(r'<header>\n *<h2><a href=\"(.*)\">(.*)<\/a><\/h2>', verge_homepage)
+  author_matches = re.findall(r'<span (?:class=\"author\")? ?itemprop=\"name\">(.*)<\/span>', verge_homepage)
+  date_matches = re.findall(r'<time class="date" .*>(.*)<\/time>', verge_homepage)
   
   #code here for creating a new web page that only displays information collected
+  header = """
+    <html>
+    <head><title>The Verge Headlines</title></head>
+    <body>
+      <table>
+        <h2>Ars Technica Latest News</h2>
+        <table><tr><th>Date</th><th>Title</th><th>Author</th></tr>
+        """
+  news = ''
+  for news_item in zip(matches, author_matches, date_matches):
+    row = '<tr><td>' + news_item[2] + '</td><td><a href="'+ news_item[0][0] + '">' + news_item[0][1] + '</a></td><td>' + news_item[1] + '</td></tr>\n        '
+    news = news + row
+  
+  footer = """
+      </table>
+     </body>
+   </html>"""
+   
+  file = open(os.path.dirname(os.path.realpath(__file__)) + '/ars.html', 'wt')
+  file.write(header+news+footer)
+  file.close()
+  
+  
+parse_arstechnica_homepage()
